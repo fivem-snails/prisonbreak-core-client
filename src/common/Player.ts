ac54211676dba74969d3ebbad314c8f800cf203f
Mailar <kodinortka@gmail.com>
2023-01-04 16:52:57 +0200

    Crouching but needs some repairs

--- src/common/Player.ts
+++ src/common/Player.ts
@@ -1,3 +1,37 @@
 on('playerSpawned', () => {
   SetPedMaxHealth(PlayerPedId(), 400);
 });
+
+// let isCrouching = false;
+
+// RegisterCommand(
+//   'crouch',
+//   () => {
+//     if (isCrouching) {
+//       if (IsPedArmed(PlayerPedId(), 6)) {
+//         ResetPedMovementClipset(PlayerPedId(), 3.0);
+//       } else {
+//         ResetPedMovementClipset(PlayerPedId(), 0.3);
+//       }
+//       isCrouching = false;
+//     } else {
+//       if (!IsPedInAnyVehicle(PlayerPedId(), false)) {
+//         ResetPedMovementClipset(PlayerPedId(), 1.0);
+//         SetPedCanPlayAmbientAnims(PlayerPedId(), false);
+//         SetPedCanPlayAmbientBaseAnims(PlayerPedId(), false);
+//         SetPedStealthMovement(PlayerPedId(), false, 'DEFAULT_ACTION');
+
+//         if (IsPedArmed(PlayerPedId(), 6)) {
+//           SetPedMovementClipset(PlayerPedId(), 'move_ped_crouched', 4.0);
+//         } else {
+//           SetPedMovementClipset(PlayerPedId(), 'move_ped_crouched', 0.3);
+//         }
+
+//         isCrouching = true;
+//       }
+//     }
+//   },
+//   false,
+// );
+
+// RegisterKeyMapping('crouch', 'Crouch', 'keyboard', 'Z');
