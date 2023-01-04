cf49cc346c13223ae5e3ca4a3a54b88e1977c60e
Mailar <kodinortka@gmail.com>
2023-01-04 14:40:45 +0200

    Zoom if player is aiming and disable roll and auto reload

--- src/common/Weapon.ts
+++ src/common/Weapon.ts
@@ -1,9 +1,17 @@
 setTick(async () => {
-  await setDelay(1);
   SetPedSuffersCriticalHits(PlayerPedId(), false);
   if (IsPedArmed(PlayerPedId(), 6)) {
     DisableControlAction(1, 140, true);
     DisableControlAction(1, 141, true);
     DisableControlAction(1, 142, true);
+  }
+});
+
+setTick(async () => {
+  if (IsPlayerFreeAiming(PlayerId())) {
+    AnimateGameplayCamZoom(1.0, 0.8);
+    DisableControlAction(0, 36, true);
+    DisableControlAction(0, 22, true);
+    SetWeaponsNoAutoreload(true);
   }
 });
