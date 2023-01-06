9c6d2bd19b32870067e3b67edc9325de2c87bbfb
Mailar <kodinortka@gmail.com>
2023-01-06 15:10:31 +0200

    Add ui to core

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,15 @@ game 'gta5'
 
 data_file 'DLC_POP_GROUPS' 'popgroups.ymt'
 
+ui_page 'src/layout/index.html'
+
+files {
+    'src/layout/index.html', 
+    'src/layout/css/index.css',
+    'src/layout/images/background.jpg',
+    'src/layout/images/map.jpg',
+}
+
 client_scripts {
     'dist/**.js'
 }
