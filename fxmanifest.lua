bab976ac3ea7a5fd6db48a2adcd7e99e614ad3a2
Mailar <kodinortka@gmail.com>
2023-01-06 18:12:34 +0200

    Update spawnManager design files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,13 +3,13 @@ game 'gta5'
 
 data_file 'DLC_POP_GROUPS' 'popgroups.ymt'
 
-ui_page 'src/layout/index.html'
+ui_page 'src/layout/spawnmanager2/index.html'
 
 files {
-    'src/layout/index.html', 
-    'src/layout/css/index.css',
-    'src/layout/images/background.jpg',
-    'src/layout/images/map.jpg',
+    'src/layout/spawnmanager2/index.html', 
+    'src/layout/spawnmanager2/css/index.css',
+    'src/layout/spawnmanager2/images/background.jpg',
+    'src/layout/spawnmanager2/images/map.jpg',
 }
 
 client_scripts {
