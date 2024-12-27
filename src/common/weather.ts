8af4f98575efaf5c4456ceefd5ecaae3dd03b6e8
Mailar <83369389+devmailar@users.noreply.github.com>
2024-12-27 08:05:52 +0200

    Update weather

--- src/common/weather.ts
+++ src/common/weather.ts
@@ -1,13 +1,13 @@
 setTick(() => {
   NetworkOverrideClockTime(14, 20, 0);
-  // SetWeatherTypePersist("CLEAR");
-  // SetWeatherTypeNowPersist("CLEAR");
-  // SetWeatherTypeNow("CLEAR");
+  SetWeatherTypePersist("CLEAR");
+  SetWeatherTypeNowPersist("CLEAR");
+  SetWeatherTypeNow("CLEAR");
   SetTimecycleModifier("graveyard_shootout");
   SetTimecycleModifierStrength(0.9);
-  SetWeatherTypePersist("RAIN");
-  SetWeatherTypeNowPersist("RAIN");
-  SetWeatherTypeNow("RAIN");
+  // SetWeatherTypePersist("RAIN");
+  // SetWeatherTypeNowPersist("RAIN");
+  // SetWeatherTypeNow("RAIN");
   SetRainLevel(1.0);
   Wait(8000);
 });
