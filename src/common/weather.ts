187e4006a17c76cdf3fda7657755b3616856e4b4
Mailar <83369389+devmailar@users.noreply.github.com>
2024-12-27 08:11:43 +0200

    Update

--- src/common/weather.ts
+++ src/common/weather.ts
@@ -3,11 +3,11 @@ setTick(() => {
   SetWeatherTypePersist("CLEAR");
   SetWeatherTypeNowPersist("CLEAR");
   SetWeatherTypeNow("CLEAR");
-  SetTimecycleModifier("graveyard_shootout");
-  SetTimecycleModifierStrength(0.9);
+  // SetTimecycleModifier("graveyard_shootout");
+  // SetTimecycleModifierStrength(0.9);
   // SetWeatherTypePersist("RAIN");
   // SetWeatherTypeNowPersist("RAIN");
   // SetWeatherTypeNow("RAIN");
-  SetRainLevel(1.0);
+  // SetRainLevel(1.0);
   Wait(8000);
 });
