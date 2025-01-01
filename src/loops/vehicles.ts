3f44b79d9c4bdc279ff406c152241d6fa28edc38
Mailar <83369389+devmailar@users.noreply.github.com>
2025-01-01 13:16:51 +0200

    Update

--- src/loops/vehicles.ts
+++ src/loops/vehicles.ts
@@ -1,5 +1,5 @@
 setTick((): void => {
   DisablePlayerVehicleRewards(PlayerId());
-  SetFlyThroughWindscreenParams(65.0, 40.0, 17.0, 2000.0);
+  SetFlyThroughWindscreenParams(65.0, 0.0, 0.0, 0.0);
   SetVehicleRadioLoud(GetVehiclePedIsIn(GetPlayerPed(-1), false), true);
 });
