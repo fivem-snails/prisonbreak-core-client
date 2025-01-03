aa71800ad01f67ad98513194cf99f533fc6d6d13
devmailar <83369389+devmailar@users.noreply.github.com>
2025-01-03 10:02:37 +0200

    Save

--- src/loops/vehicles.ts
+++ src/loops/vehicles.ts
@@ -48,46 +48,13 @@ setTick(async (): Promise<void> => {
       const playerIndex: number = GetPlayerIndex();
       const playerSrc: number = GetPlayerServerId(playerIndex);
 
-      emitNet("Core/Server/Shared:GetVehicle", playerSrc, serverVehicleModelName);
-
-      const serverVehiclePrice: string = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(
-        5_000_000,
+      emitNet(
+        "Core/Server/Shared:GetVehicle",
+        playerSrc,
+        serverVehicleModelName,
+        serverVehicleScreenX,
+        serverVehicleScreenY,
       );
-
-      const rectWidth: number = 0.07;
-      const rectHeight: number = 0.04;
-
-      DrawRect(serverVehicleScreenX, serverVehicleScreenY + rectHeight / 2, rectWidth, rectHeight, 0, 0, 0, 150);
-
-      // Draw vehicle model text
-      BeginTextCommandDisplayText("STRING");
-      SetTextScale(0.0, 0.16);
-      SetTextFont(0);
-      SetTextProportional(true);
-      SetTextCentre(true);
-      SetTextColour(255, 255, 255, 100);
-      SetTextDropshadow(0, 0, 0, 0, 255);
-      SetTextEdge(1, 0, 0, 0, 255);
-      SetTextDropShadow();
-      SetTextOutline();
-      SetTextEntry("STRING");
-      AddTextComponentString(`${serverVehicleModelName}`);
-      EndTextCommandDisplayText(serverVehicleScreenX, serverVehicleScreenY + rectHeight / 2 - 0.015);
-
-      // Draw vehicle price text
-      BeginTextCommandDisplayText("STRING");
-      SetTextScale(0.0, 0.2);
-      SetTextFont(0);
-      SetTextProportional(true);
-      SetTextCentre(true);
-      SetTextColour(0, 255, 0, 255);
-      SetTextDropshadow(0, 0, 0, 0, 250);
-      SetTextEdge(1, 0, 0, 0, 255);
-      SetTextDropShadow();
-      SetTextOutline();
-      SetTextEntry("STRING");
-      AddTextComponentString(serverVehiclePrice);
-      EndTextCommandDisplayText(serverVehicleScreenX, serverVehicleScreenY + rectHeight / 2);
     }
   });
 });
