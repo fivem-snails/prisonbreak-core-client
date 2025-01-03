ef787ec63580be5477fa265d5b3041201fa4377c
devmailar <83369389+devmailar@users.noreply.github.com>
2025-01-03 13:58:49 +0200

    Save

--- src/event/baseevents/onPlayerKilled.ts
+++ src/event/baseevents/onPlayerKilled.ts
@@ -1,13 +1,19 @@
-// on(
-//   "baseevents:onPlayerKilled",
-//   (
-//     _killerId: number,
-//     _weaponHash: string,
-//     _killerInVeh: boolean,
-//     _killerVehSeat: number,
-//     _killerVehName: string,
-//     _deathCoords: Array<number>,
-//   ) => {
-//     emit("Screens/death", true, 100);
-//   },
-// );
+on(
+  "baseevents:onPlayerKilled",
+  (
+    killerId: number,
+    _weaponHash: string,
+    _killerInVeh: boolean,
+    _killerVehSeat: number,
+    _killerVehName: string,
+    _deathCoords: Array<number>,
+  ) => {
+    try {
+      emit("baseevents:onPlayerDied", killerId, []);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  },
+);
