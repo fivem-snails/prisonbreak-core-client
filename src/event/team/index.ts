0b5aa719914ba78694c1d6a75b9589d8af7e6d42
devmailar <83369389+devmailar@users.noreply.github.com>
2025-01-05 10:39:09 +0200

    save

--- src/event/team/index.ts
+++ src/event/team/index.ts
@@ -9,6 +9,7 @@ const TeamAssign = (team: string): void => {
 
   console.info("Player team is now: ", GetPedRelationshipGroupHash(PlayerPedId()));
 
+  DistantCopCarSirens(false);
   PlaySoundFrontend(-1, "Popup_Confirm_Success", "GTAO_Exec_SecuroServ_Computer_Sounds", false);
   emit("alta-nui-welcome", true);
 };
