03495a84e0eb658ce0dba09915f96417aab73583
devmailar <83369389+devmailar@users.noreply.github.com>
2025-01-05 10:40:54 +0200

    Save

--- src/event/team/index.ts
+++ src/event/team/index.ts
@@ -1,4 +1,4 @@
-const TeamAssign = (team: string): void => {
+const TeamAssign = async (team: string): Promise<void> => {
   const groupAlreadyExists: boolean = DoesRelationshipGroupExist(team.toUpperCase());
   if (!groupAlreadyExists) {
     throw new Error(`Relationship group ${team.toUpperCase()} does not exist`);
@@ -8,6 +8,8 @@ const TeamAssign = (team: string): void => {
   SetPedRelationshipGroupHash(PlayerPedId(), team.toUpperCase());
 
   console.info("Player team is now: ", GetPedRelationshipGroupHash(PlayerPedId()));
+
+  await AddDelay(4000);
 
   DistantCopCarSirens(false);
   PlaySoundFrontend(-1, "Popup_Confirm_Success", "GTAO_Exec_SecuroServ_Computer_Sounds", false);
