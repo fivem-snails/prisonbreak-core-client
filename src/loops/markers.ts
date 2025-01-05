dbdad2bd3f71a66c17f08812a6b53fc82a7be35c
devmailar <83369389+devmailar@users.noreply.github.com>
2025-01-05 10:18:16 +0200

    Save

---
+++ src/loops/markers.ts
@@ -0,0 +1,29 @@
+setTick((): void => {
+  DrawMarker(
+    36,
+    1854.62,
+    2616.28,
+    45.67,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1.5,
+    1.5,
+    1.5,
+    0,
+    144,
+    247,
+    0.8,
+    false,
+    false,
+    2,
+    false,
+    // @ts-ignore
+    null,
+    null,
+    false,
+  );
+});
