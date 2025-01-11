757ee55d8417bbb74f72af666a00b35a264ff038
devmailar <83369389+devmailar@users.noreply.github.com>
2025-01-11 17:44:25 +0200

    Save

--- src/event/team/index.ts
+++ src/event/team/index.ts
@@ -1,6 +1,7 @@
 const TeamAssign = (team: string): void => {
-  SetTimecycleModifier("hud_def_blur");
-
+  SetTimecycleModifier("BloomMid");
+  SetTimecycleModifierStrength(3.0);
+  
   const groupAlreadyExists: boolean = DoesRelationshipGroupExist(team.toUpperCase());
   if (!groupAlreadyExists) {
     throw new Error(`Relationship group ${team.toUpperCase()} does not exist`);
