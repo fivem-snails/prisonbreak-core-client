770415bfeb4c7150cf21ce502af102e2b15694e4
devmailar <83369389+devmailar@users.noreply.github.com>
2025-01-19 11:32:59 +0200

    save

--- src/event/player/joining.ts
+++ src/event/player/joining.ts
@@ -2,7 +2,7 @@ const PlayerJoining = (playerJoiningName: string): void => {
   try {
     PlaySoundFrontend(-1, "Popup_Confirm_Success", "GTAO_Exec_SecuroServ_Computer_Sounds", false);
     BeginTextCommandThefeedPost("STRING");
-    AddTextComponentSubstringPlayerName(`~g~${playerJoiningName} ~w~joined the game.`);
+    AddTextComponentSubstringPlayerName(`~c~${playerJoiningName} joined the game.`);
     EndTextCommandThefeedPostTicker(true, true);
   } catch (error: unknown) {
     if (error instanceof Error) {
