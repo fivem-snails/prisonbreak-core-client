e5eb5002bc6c049c0bf6473ebc22dfd60d176b19
devmailar <83369389+devmailar@users.noreply.github.com>
2025-01-25 17:36:28 +0200

    Save

--- src/loops/teams.ts
+++ src/loops/teams.ts
@@ -1,23 +1,23 @@
 // Make the loop exec after an event is called
 
-setTick((): void => {
-  const serverPlayers: number[] = GetActivePlayers();
+// setTick((): void => {
+//   const serverPlayers: number[] = GetActivePlayers();
 
-  for (const serverPlayerSID of serverPlayers) {
-    const serverPlayerPed: number = GetPlayerPed(serverPlayerSID);
+//   for (const serverPlayerSID of serverPlayers) {
+//     const serverPlayerPed: number = GetPlayerPed(serverPlayerSID);
 
-    if (serverPlayerPed !== PlayerPedId()) {
-      const serverPlayerBlip: number = AddBlipForEntity(serverPlayerPed);
-      const serverPlayerGroupHash: number = GetPedRelationshipGroupHash(serverPlayerPed);
-      const serverPlayerBlipName: string = serverPlayerGroupHash === -1185955016 ? "Criminal" : "Police";
-      const serverPlayerBlipColour: number = serverPlayerGroupHash === -1185955016 ? 44 : 42;
+//     if (serverPlayerPed !== PlayerPedId()) {
+//       const serverPlayerBlip: number = AddBlipForEntity(serverPlayerPed);
+//       const serverPlayerGroupHash: number = GetPedRelationshipGroupHash(serverPlayerPed);
+//       const serverPlayerBlipName: string = serverPlayerGroupHash === -1185955016 ? "Criminal" : "Police";
+//       const serverPlayerBlipColour: number = serverPlayerGroupHash === -1185955016 ? 44 : 42;
 
-      SetBlipSprite(serverPlayerBlip, 1);
-      SetBlipColour(serverPlayerBlip, serverPlayerBlipColour);
-      SetBlipAsShortRange(serverPlayerBlip, true);
-      BeginTextCommandSetBlipName("STRING");
-      AddTextComponentString(`${serverPlayerBlipName} [${serverPlayerSID}]`);
-      EndTextCommandSetBlipName(serverPlayerBlip);
-    }
-  }
-});
+//       SetBlipSprite(serverPlayerBlip, 1);
+//       SetBlipColour(serverPlayerBlip, serverPlayerBlipColour);
+//       SetBlipAsShortRange(serverPlayerBlip, true);
+//       BeginTextCommandSetBlipName("STRING");
+//       AddTextComponentString(`${serverPlayerBlipName} [${serverPlayerSID}]`);
+//       EndTextCommandSetBlipName(serverPlayerBlip);
+//     }
+//   }
+// });
