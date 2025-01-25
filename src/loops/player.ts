c5dce20a3be1dafe2970d97acf91cbef6d82980f
devmailar <83369389+devmailar@users.noreply.github.com>
2025-01-25 12:55:10 +0200

    save

--- src/loops/player.ts
+++ src/loops/player.ts
@@ -17,23 +17,6 @@ setTick(() => {
 
 setTick((): void => {
   if (IsControlPressed(0, 20)) {
-    const serverPlayersIndices: number[] = GetActivePlayers();
-
-    const serverPlayers: IPlayer[] = serverPlayersIndices.map((serverPlayerIndice: number): IPlayer => {
-      const serverPlayerSID: number = GetPlayerServerId(serverPlayerIndice);
-      const serverPlayerGroup: number = GetPlayerGroup(serverPlayerIndice);
-
-      return {
-        sid: serverPlayerSID,
-        group: serverPlayerGroup,
-      };
-    });
-
-    console.info("serverPlayers:", {
-      serverPlayersIndices,
-      serverPlayers,
-    });
-
     emit("prisonbreak-nui-scoreboard", true);
   } else {
     emit("prisonbreak-nui-scoreboard", false);
