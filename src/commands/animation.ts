df43b32c0008d0b838c21de03f937f273b976733
devmailar <83369389+devmailar@users.noreply.github.com>
2025-02-03 16:59:32 +0200

    save

--- src/commands/animation.ts
+++ src/commands/animation.ts
@@ -3,23 +3,22 @@ RegisterCommand(
   async (source: number, args: string[], rawCommand: string): Promise<void> => {
     try {
       const playerIndex: number = GetPlayerIndex();
-      const playerSrc: number = GetPlayerServerId(playerIndex);
 
       console.info("animation", {
         source,
-        playerSrc,
+        playerIndex,
         arg: args[0],
         rawCommand,
       });
 
-      ClearPedTasksImmediately(GetPlayerPed(playerSrc));
+      ClearPedTasksImmediately(GetPlayerPed(playerIndex));
 
       RequestAnimDict("mp_arresting");
       if (!HasAnimDictLoaded("mp_arresting")) {
         await Waiit(1000);
       }
 
-      TaskPlayAnim(GetPlayerPed(playerSrc), "mp_arresting", args[0], 1.0, 1.0, 6000, 49, 1.0, true, true, true);
+      TaskPlayAnim(GetPlayerPed(playerIndex), "mp_arresting", args[0], 1.0, 1.0, 6000, 49, 1.0, true, true, true);
     } catch (error: unknown) {
       if (error instanceof Error) {
         console.error(error.message);
