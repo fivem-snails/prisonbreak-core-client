c22e33cbb269dcd10a350069516a075f200eaf2e
devmailar <83369389+devmailar@users.noreply.github.com>
2025-02-03 16:54:06 +0200

    save

--- src/commands/animation.ts
+++ src/commands/animation.ts
@@ -1,12 +1,19 @@
 RegisterCommand(
   "animation",
-  (playerSrc: number, args: string[], rawCommand: string): void => {
+  async (playerSrc: number, args: string[], rawCommand: string): Promise<void> => {
     try {
       console.info("animation", {
         playerSrc,
         args,
         rawCommand,
       });
+
+      ClearPedTasksImmediately(GetPlayerPed(playerSrc));
+
+      RequestAnimDict("mp_arresting");
+      if (!HasAnimDictLoaded("mp_arresting")) {
+        await Waiit(1000);
+      }
 
       TaskPlayAnim(GetPlayerPed(playerSrc), "mp_arresting", args[0], 1.0, 1.0, 8000, 1, 1.0, true, true, true);
     } catch (error: unknown) {
