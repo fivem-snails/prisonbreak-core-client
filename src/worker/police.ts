d26b2cc8c04eda2d0ee3047c1e45efa0fa0c37ce
devmailar <83369389+devmailar@users.noreply.github.com>
2025-02-04 18:09:28 +0200

    Big fix

--- src/worker/police.ts
+++ src/worker/police.ts
@@ -25,3 +25,75 @@ setTick(async (): Promise<void> => {
     EnableDispatchService(id, false);
   }
 });
+
+setTick(async (): Promise<void> => {
+  await Waiit(1000);
+
+  const authorIndex: number = GetPlayerIndex();
+  const authorSrc: number = GetPlayerServerId(authorIndex);
+  const authorPed: number = PlayerPedId();
+  const authorCoords: number[] = GetEntityCoords(authorPed, false);
+
+  const authorRelationshipGroupHash: number = GetPedRelationshipGroupHash(authorPed);
+  const authorRelationshipGroup: "CRIMINAL" | "POLICE" =
+    authorRelationshipGroupHash === -1185955016 ? "CRIMINAL" : "POLICE";
+
+  const isAuthorGroupOfPolice: boolean = authorRelationshipGroup === "POLICE";
+
+  if (isAuthorGroupOfPolice) {
+    const targets: number[] = GetActivePlayers();
+
+    for (const targetIndex of targets) {
+      const targetSrc: number = GetPlayerServerId(targetIndex);
+      const targetPed: number = GetPlayerPed(targetIndex);
+
+      if (targetPed !== authorPed) {
+        const targetCoords: number[] = GetEntityCoords(targetPed, false);
+
+        const distanceFromAuthorToTarget: number = GetDistanceBetweenCoords(
+          authorCoords[0],
+          authorCoords[1],
+          authorCoords[2],
+          targetCoords[0],
+          targetCoords[1],
+          targetCoords[2],
+          true,
+        );
+
+        if (distanceFromAuthorToTarget < 0.8) {
+          RequestAnimDict("mp_arresting");
+          if (!HasAnimDictLoaded("mp_arresting")) {
+            await Waiit(500);
+          }
+
+          TaskPlayAnim(
+            GetPlayerPed(targetIndex),
+            "mp_arresting",
+            "arrested_spin_l_0",
+            1.0,
+            1.0,
+            4000,
+            49,
+            1.0,
+            true,
+            true,
+            true,
+          );
+
+          console.warn(`[${targetSrc}] was arrested by [${authorSrc}]`);
+
+          // Send to prison (fadeOut) and freeze, no server-wide animation needed for now
+
+          emitNet(
+            "prisonbreak-core-client:event:police:handcuff",
+            authorSrc,
+            authorIndex,
+            targetSrc,
+            targetIndex,
+            targetPed,
+          );
+        }
+      }
+    }
+  }
+});
