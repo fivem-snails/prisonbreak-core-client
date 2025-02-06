b5d0e1e5ea1bbe54acdcde04e591ad48da31d937
devmailar <83369389+devmailar@users.noreply.github.com>
2025-02-06 18:18:44 +0200

    Disable loop

--- src/worker/police.ts
+++ src/worker/police.ts
@@ -26,88 +26,88 @@ setTick(async (): Promise<void> => {
   }
 });
 
-setTick(async (): Promise<void> => {
-  const authorIndex: number = GetPlayerIndex();
-  const authorSrc: number = GetPlayerServerId(authorIndex);
-  const authorPed: number = PlayerPedId();
-  const authorCoords: number[] = GetEntityCoords(authorPed, false);
+// setTick(async (): Promise<void> => {
+//   const authorIndex: number = GetPlayerIndex();
+//   const authorSrc: number = GetPlayerServerId(authorIndex);
+//   const authorPed: number = PlayerPedId();
+//   const authorCoords: number[] = GetEntityCoords(authorPed, false);
 
-  const authorRelationshipGroupHash: number = GetPedRelationshipGroupHash(authorPed);
-  const authorRelationshipGroup: "CRIMINAL" | "POLICE" =
-    authorRelationshipGroupHash === -1185955016 ? "CRIMINAL" : "POLICE";
+//   const authorRelationshipGroupHash: number = GetPedRelationshipGroupHash(authorPed);
+//   const authorRelationshipGroup: "CRIMINAL" | "POLICE" =
+//     authorRelationshipGroupHash === -1185955016 ? "CRIMINAL" : "POLICE";
 
-  const isAuthorGroupOfPolice: boolean = authorRelationshipGroup === "POLICE";
+//   const isAuthorGroupOfPolice: boolean = authorRelationshipGroup === "POLICE";
 
-  if (isAuthorGroupOfPolice) {
-    const targets: number[] = GetActivePlayers();
+//   if (isAuthorGroupOfPolice) {
+//     const targets: number[] = GetActivePlayers();
 
-    for (const targetIndex of targets) {
-      const targetSrc: number = GetPlayerServerId(targetIndex);
-      const targetPed: number = GetPlayerPed(targetIndex);
+//     for (const targetIndex of targets) {
+//       const targetSrc: number = GetPlayerServerId(targetIndex);
+//       const targetPed: number = GetPlayerPed(targetIndex);
 
-      if (targetPed !== authorPed) {
-        const targetCoords: number[] = GetEntityCoords(targetPed, false);
+//       if (targetPed !== authorPed) {
+//         const targetCoords: number[] = GetEntityCoords(targetPed, false);
 
-        const distanceFromAuthorToTarget: number = GetDistanceBetweenCoords(
-          authorCoords[0],
-          authorCoords[1],
-          authorCoords[2],
-          targetCoords[0],
-          targetCoords[1],
-          targetCoords[2],
-          true,
-        );
+//         const distanceFromAuthorToTarget: number = GetDistanceBetweenCoords(
+//           authorCoords[0],
+//           authorCoords[1],
+//           authorCoords[2],
+//           targetCoords[0],
+//           targetCoords[1],
+//           targetCoords[2],
+//           true,
+//         );
 
-        if (distanceFromAuthorToTarget < 0.8) {
-          RequestAnimDict("mp_arrest_paired");
-          if (!HasAnimDictLoaded("mp_arrest_paired")) {
-            await Waiit(500);
-          }
+//         if (distanceFromAuthorToTarget < 0.8) {
+//           RequestAnimDict("mp_arrest_paired");
+//           if (!HasAnimDictLoaded("mp_arrest_paired")) {
+//             await Waiit(500);
+//           }
 
-          TaskPlayAnim(authorPed, "mp_arrest_paired", "cop_p2_back_left", 1.0, 1.0, 6000, 49, 1.0, true, true, true);
+//           TaskPlayAnim(authorPed, "mp_arrest_paired", "cop_p2_back_left", 1.0, 1.0, 6000, 49, 1.0, true, true, true);
 
-          RequestAnimDict("mp_arresting");
-          if (!HasAnimDictLoaded("mp_arresting")) {
-            await Waiit(500);
-          }
+//           RequestAnimDict("mp_arresting");
+//           if (!HasAnimDictLoaded("mp_arresting")) {
+//             await Waiit(500);
+//           }
 
-          TaskPlayAnim(targetPed, "mp_arrest_paired", "crook_p2_back_left", 1.0, 1.0, 6000, 49, 1.0, true, true, true);
-          AttachEntityToEntity(
-            targetPed,
-            authorPed,
-            11816,
-            -0.1,
-            0.45,
-            0.0,
-            0.0,
-            0.0,
-            20.0,
-            false,
-            false,
-            false,
-            false,
-            20,
-            false,
-          );
+//           TaskPlayAnim(targetPed, "mp_arrest_paired", "crook_p2_back_left", 1.0, 1.0, 6000, 49, 1.0, true, true, true);
+//           AttachEntityToEntity(
+//             targetPed,
+//             authorPed,
+//             11816,
+//             -0.1,
+//             0.45,
+//             0.0,
+//             0.0,
+//             0.0,
+//             20.0,
+//             false,
+//             false,
+//             false,
+//             false,
+//             20,
+//             false,
+//           );
 
-          console.warn(`[${targetSrc}] was arrested by [${authorSrc}]`);
+//           console.warn(`[${targetSrc}] was arrested by [${authorSrc}]`);
 
-          DetachEntity(targetPed, true, false);
+//           DetachEntity(targetPed, true, false);
 
-          // Send to prison (fadeOut) and freeze, no server-wide animation needed for now
+//           // Send to prison (fadeOut) and freeze, no server-wide animation needed for now
 
-          emitNet(
-            "prisonbreak-core-client:event:police:handcuff",
-            authorSrc,
-            authorIndex,
-            targetSrc,
-            targetIndex,
-            targetPed,
-          );
-        }
-      }
-    }
-  }
+//           emitNet(
+//             "prisonbreak-core-client:event:police:handcuff",
+//             authorSrc,
+//             authorIndex,
+//             targetSrc,
+//             targetIndex,
+//             targetPed,
+//           );
+//         }
+//       }
+//     }
+//   }
 
-  await Waiit(10000);
-});
+//   await Waiit(10000);
+// });
