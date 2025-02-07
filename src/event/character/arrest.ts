9cbf6b6dc6495f3ab5057362cba9c86e5bdb133c
devmailar <83369389+devmailar@users.noreply.github.com>
2025-02-07 15:54:49 +0200

    Disgusting code

--- src/event/character/arrest.ts
+++ src/event/character/arrest.ts
@@ -1,21 +1,9 @@
-const CharacterArrest = async (
-  authorSrc: number,
-  authorIndex: number,
-  targetSrc: number,
-  targetIndex: number,
-): Promise<void> => {
+const CharacterArrest = async (authorIndex: number, targetIndex: number): Promise<void> => {
   try {
     RequestAnimDict("mp_arrest_paired");
     if (!HasAnimDictLoaded("mp_arrest_paired")) {
       await Waiit(500);
     }
-
-    console.info("CharacterArrest", {
-      authorSrc,
-      authorIndex,
-      targetSrc,
-      targetIndex,
-    });
 
     TaskPlayAnim(
       GetPlayerPed(authorIndex),
@@ -63,28 +51,6 @@ const CharacterArrest = async (
     if (isDispatcherGroupOfPolice) {
       emit("prisonbreak-core-client:event:player:message", "~c~GG! You just arrested a criminal and received ~g~$200");
     }
-
-    // AttachEntityToEntity(
-    //   targetPed,
-    //   GetPlayerPed(-1),
-    //   11816,
-    //   -0.1,
-    //   0.45,
-    //   0,
-    //   0,
-    //   0,
-    //   20,
-    //   false,
-    //   false,
-    //   false,
-    //   false,
-    //   20,
-    //   false,
-    // );
-
-    // await Waiit(7000);
-
-    // DetachEntity(GetPlayerPed(-1), true, false);
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error(error.message);
