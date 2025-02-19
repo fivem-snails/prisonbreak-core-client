1640e5ea1a120f7a24131422efe9592832d842b6
devmailar <83369389+devmailar@users.noreply.github.com>
2025-02-19 14:54:50 +0200

    Save  side vehicles worker.

--- src/worker/vehicles.ts
+++ src/worker/vehicles.ts
@@ -1,4 +1,6 @@
-setTick(async (): Promise<void> => {
+const visitedVehicles: Set<number> = new Set();
+
+setTick(async () => {
   DisablePlayerVehicleRewards(PlayerId());
   SetFlyThroughWindscreenParams(30.0, 0.0, 0.0, 0.0);
   SetVehicleRadioLoud(GetVehiclePedIsIn(GetPlayerPed(-1), false), true);
@@ -6,8 +8,8 @@ setTick(async (): Promise<void> => {
   const playerCoords = GetEntityCoords(PlayerPedId(), true);
   const vehicles: number[] = GetGamePool("CVehicle");
 
-  vehicles.map(async (serverVehicle: number): Promise<void> => {
-    const vehicleCoords = GetEntityCoords(serverVehicle, true);
+  vehicles.map(async (vehicle) => {
+    const vehicleCoords = GetEntityCoords(vehicle, true);
     const distancefromPlayerToVehicle = GetDistanceBetweenCoords(
       playerCoords[0],
       playerCoords[1],
@@ -18,27 +20,27 @@ setTick(async (): Promise<void> => {
       true,
     );
 
-    // console.info("Vehicle Data:", {
-    //   serverVehicle,
-    //   serverVehicleCoords,
-    //   distancefromPlayerToServerVehicle,
-    // });
-
-    if (distancefromPlayerToVehicle < 2) {
+    if (distancefromPlayerToVehicle < 2 && !visitedVehicles.has(vehicle)) {
       const [_, vehicleScreenX, vehicleScreenY] = GetScreenCoordFromWorldCoord(vehicleCoords[0], vehicleCoords[1], vehicleCoords[2] + 1.0);
-      const vehicleModel = GetEntityModel(serverVehicle);
-      const vehicleModelName = GetDisplayNameFromVehicleModel(vehicleModel).toLowerCase();
-      const rectHeight = 0.04;
+      const vehicleEntity = GetEntityModel(vehicle);
+      const vehicleModel = GetDisplayNameFromVehicleModel(vehicleEntity).toLowerCase();
 
-      const playerIndex = GetPlayerIndex();
-      const playerSrc = GetPlayerServerId(playerIndex);
+      console.log("VEHICLE_NUI");
 
-      emitNet("Core/Server/Shared:GetVehicle", playerSrc, vehicleModelName, vehicleScreenX, vehicleScreenY, rectHeight);
+      emit("NUI_VEHICLEBUY", true, vehicleModel);
+
+      return visitedVehicles.add(vehicle);
+    }
+
+    if (distancefromPlayerToVehicle > 2 && visitedVehicles.has(vehicle)) {
+      console.log("VEHICLE_DELETE");
+
+      return visitedVehicles.delete(vehicle);
     }
   });
 });
 
-setTick((): void => {
+setTick(() => {
   if (IsPedOnFoot(GetPlayerPed(-1))) {
     SetRadarZoom(1100);
   } else if (IsPedInAnyVehicle(GetPlayerPed(-1), true)) {
